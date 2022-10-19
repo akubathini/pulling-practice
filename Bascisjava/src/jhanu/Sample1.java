6498f62b8dc2ed2aae70ffaa1773c2e45e2e0e66
akubathini <jhansiakubathini97@gmail.com>
2022-10-19 13:15:35 +0530

    sample1

---
+++ Bascisjava/src/jhanu/Sample1.java
@@ -0,0 +1,24 @@
+package jhanu;
+
+public class Sample1 {
+
+	public static void main(String[] args)
+	{
+		
+  int i=10;
+  double d=10.445;
+  char c='a';
+  String s="welcome";
+  boolean b=true;
+  System.out.println(i);
+  System.out.println(d);
+  System.out.println(c);
+  System.out.println(b);
+  System.out.println(s);
+//TODO Auto-generated method stub
+  
+  
+
+	}
+
+}
